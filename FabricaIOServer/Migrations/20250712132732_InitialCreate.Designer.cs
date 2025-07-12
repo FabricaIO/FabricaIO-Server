43c4e586e5bf1b5b34dc0a02e48dea19e07a1152
Sam Groveman <samgroveman@outlook.com>
2025-07-12 09:28:12 -0400

    Emptied database

--- FabricaIOServer/Migrations/20250712132732_InitialCreate.Designer.cs
+++ FabricaIOServer/Migrations/20250712132732_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FabricaIOServer.Migrations
 {
     [DbContext(typeof(DeviceContext))]
-    [Migration("20250712125803_InitialCreate")]
+    [Migration("20250712132732_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
